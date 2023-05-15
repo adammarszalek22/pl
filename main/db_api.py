f3b72b97c025b4439f6e02e348f8ceb29b450e20
Adam Marszalek <adammarszalek60@gmail.com>
2023-05-15 16:59:42 +0100

    First commit

---
+++ main/db_api.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+url = "http://127.0.0.1:5000"
+
+def create_user2(username, password):
+    user = requests.post(url + '/register', json={"username": username, "password": password})
+    return json.loads(user.text)
+
+def login(username, password):
+    request = requests.post(url + '/login', json={"username": username, "password": password})
+    access_token = json.loads(request.text)["access_token"]
+    refresh_token = json.loads(request.text)["refresh_token"]
+    user_id = json.loads(request.text)["user_id"]
+    return [access_token, refresh_token, user_id]
+
+def revoke_jwt(username, password):
+    user = requests.post(url + '/logout', json={"username": username, "password": password})
+    return json.loads(user.text)
+
+def get_non_fresh_token(refresh_token, username, password):
+    request = requests.post(url + '/refresh', headers={"Authorization": "Bearer " + refresh_token},
+                         json={"username": username, "password": password})
+    new_access_token = json.loads(request.text)["access_token"]
+    return new_access_token
+
+def my_user_info(access_token, id):
+    user = requests.get(url + '/user/' + str(id), headers={"Authorization": "Bearer " + access_token})
+    return json.loads(user.text)
+
+def get_all_users(access_token):
+    users = requests.get(url + '/get_all', headers={"Authorization": "Bearer " + access_token})
+    return json.loads(users.text)
+
+def delete_account(access_token, id):
+    delete = requests.delete(url + "/user/" + str(id), headers={"Authorization": "Bearer " + access_token})
+    return json.loads(delete.text)
+
+def get_all_bets(access_token):
+    bets = requests.get(url + '/bet', headers={"Authorization": "Bearer " + access_token})
+    return json.loads(bets.text)
+
+def get_bet(access_token, id):
+    bet = requests.get(url + '/bet/' + str(id), headers={"Authorization": "Bearer " + access_token})
+    return json.loads(bet.text)
+
+def update_bet(access_token, id, goal1, goal2):
+    new_bet = requests.put(url + '/bet/' + str(id), headers={"Authorization": "Bearer " + access_token},
+                           json={"goal1": goal1, "goal2": goal2})
+    return json.loads(new_bet.text)
+
+def delete_bet(access_token, id):
+    bet = requests.delete(url + '/bet/' + str(id), headers={"Authorization": "Bearer " + access_token})
+    return json.loads(bet.text)
+
+def post_bet(access_token, match_id, goal1, goal2, user_id):
+    bet = requests.post(url + '/bet', headers={"Authorization": "Bearer " + access_token},
+                        json={"match_id": match_id, "goal1": goal1, "goal2": goal2, "user_id": user_id})
+    return bet
+
+#a = login("adam", "1234")[0]
+#post_bet(a, "2293058", 3, 4, 1)
---
+++ main/main.py
@@ -0,0 +1,217 @@
+from login import *
+from pl_api import *
+from db_api import *
+from kivy.uix.textinput import TextInput
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.textfield import MDTextFieldRect, MDTextField
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import NoTransition
+from kivy.clock import Clock
+
+
+class MyLabel(MDLabel):
+    pass
+
+class LoginWindow(Screen):
+    def on_entry(self):
+        on_entry()
+
+    def log_in(self):
+        username = self.ids.login.text
+        password = self.ids.password.text
+        if check_password(username, password) == True:
+            self.manager.transition = NoTransition()
+            self.manager.current = 'PLTable'
+            app = MDApp.get_running_app()
+            app.access_token, app.refresh_token, app.user_id = login(username, password)
+        else:
+            print('Wrong password')
+    def password(self):
+        if self.ids.password.password == True:
+            self.ids.password.password = False
+            self.ids.password.icon_left = "eye"
+            Clock.schedule_once(self.focus, 0.05)
+        else:
+            self.ids.password.password = True
+            self.ids.password.icon_left = "eye-off"
+            Clock.schedule_once(self.focus, 0.05)
+    
+    def focus(self, dt):
+        self.ids.password.focus = True
+'''
+class LoginWindow1(Screen):
+    pass
+
+    def log_in(self):
+        username = self.ids.login.text
+        password = self.ids.password.text
+        if check_password(username, password) == True:
+            self.manager.transition = NoTransition()
+            self.manager.current = 'PLTable'
+        else:
+            pass
+            #self.ids.my_label.text = 'Failed to log in'
+            #self.ids.my_label.height = self.ids.my_label.texture_size[1]
+'''
+class CreateUser(Screen):
+    pass
+
+    def create_user_app(self):
+        username = self.ids.login.text
+        password = self.ids.password.text
+        if check_password(username, password):
+            create_user(username, password)
+            login(username, password)
+            #add_player(username)
+            self.manager.current = 'PLTable'
+        else:
+            self.ids.my_label.text = 'User already exists'
+            self.ids.my_label.height = self.ids.my_label.texture_size[1]
+
+class PLTable(Screen):
+    pass
+
+    def table(self):
+        name = ''
+        matches_played = ''
+        wins = ''
+        draws = ''
+        losses = ''
+        goals_scored = ''
+        goals_conceded = ''
+        goals_balance = ''
+        points = ''
+        for i in positions.keys():
+            name += positions[i]["name"] + '\n'
+            matches_played += str(positions[i]["matches_played"]) + '\n'
+            wins += str(positions[i]["wins"]) + '\n'
+            draws += str(positions[i]["draws"]) + '\n'
+            losses += str(positions[i]["losses"]) + '\n'
+            goals_scored += str(positions[i]["goals_scored"]) + '\n'
+            goals_conceded += str(positions[i]["goals_conceded"]) + '\n'
+            goals_balance += str(positions[i]["goals_balance"]) + '\n'
+            points += str(positions[i]["points"]) + '\n'
+        self.ids.name.text = name
+        self.ids.matches_played.text = matches_played
+        self.ids.wins.text = wins 
+        self.ids.draws.text = draws
+        self.ids.losses.text = losses
+        self.ids.goals_scored.text = goals_scored
+        self.ids.goals_conceded.text = goals_conceded
+        self.ids.goals_balance.text = goals_balance
+        self.ids.points.text = points
+
+class Bet(Screen):
+    pass
+
+    def bets(self):
+        
+        b = 0
+        for i in matches.keys():
+            for i2 in matches[i].keys():
+                if matches[i][i2]["started"] == False:
+                    a = i
+                    b = 1
+                    break
+            if b == 1:
+                break
+
+        list1 = []
+        self.codes = {}
+
+        for i in matches[a].keys():
+            list1.append(i)
+
+        app = MDApp.get_running_app()
+        layout = self.ids.mylayout
+        layout_height = app.root.height / (len(list1) - 2)
+
+        self.entries = []
+        self.codes["Gameweek"] = a
+        self.codes["codes"] = {}
+
+        for i in list1:
+            
+            gridlayout = MDGridLayout(size_hint_y = None, height = layout_height, cols = 6)
+
+            self.team1 = MyLabel(text = str(teams[matches[a][i]["team1"]]["name"]),
+                                 valign = "middle",
+                                 halign = "center")
+            gridlayout.add_widget(self.team1)
+
+
+            self.goal1 = MyLabel(text = str(matches[a][i]["goals1"]),
+                                 valign = "middle",
+                                 halign = "center")
+            gridlayout.add_widget(self.goal1)
+
+
+            self.guess1 = MDTextField(size_hint = [0.5, 1],
+                                      multiline = False,
+                                      input_type = 'number')
+            gridlayout.add_widget(self.guess1)
+            self.entries.append(self.guess1)
+
+            self.guess2 = MDTextField(size_hint = [0.5, 1],
+                                      multiline = False,
+                                      input_type = 'number')
+            gridlayout.add_widget(self.guess2)
+            self.entries.append(self.guess2)
+            
+
+            self.goal2 = MyLabel(text = str(matches[a][i]["goals2"]),
+                                 valign = "middle",
+                                 halign = "center")
+            gridlayout.add_widget(self.goal2)
+
+
+            self.team2 = MyLabel(text=str(teams[matches[a][i]["team2"]]["name"]),
+                                 valign = "middle",
+                                 halign = "center")
+            gridlayout.add_widget(self.team2)
+
+            
+            self.codes["codes"][i] = {}
+            self.codes["codes"][i]["guess1"] = self.guess1
+            self.codes["codes"][i]["guess2"] = self.guess2
+
+            layout.add_widget(gridlayout)
+    
+    def do_bets(self):
+        for i in self.entries:
+            if i.text == '':
+                i.text = '0'
+        app = MDApp.get_running_app()
+        for i in self.codes["codes"].keys():
+            print(i)
+            guess1 = int(self.codes["codes"][i]["guess1"].text)
+            guess2 = int(self.codes["codes"][i]["guess2"].text)
+            post_bet(str(app.access_token), str(i), guess1, guess2, int(app.user_id))
+
+        
+
+
+class MainWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+class AwesomeApp(MDApp):
+    def build(self):
+        kv = Builder.load_file('user.kv')
+        LoginWindow.on_entry(LoginWindow)
+        #Window.clearcolor = (1, 1, 1, 1)
+        self.theme_cls.theme_style = 'Light'
+        self.theme_cls.primary_palette = 'Teal'
+        #self.theme_cls.accent_palette = ''
+        return kv
+
+if __name__ == '__main__':
+    AwesomeApp().run()
