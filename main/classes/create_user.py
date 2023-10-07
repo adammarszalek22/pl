15a2e0e3cedd08a0f3944f0189173e966cb70c10
adammarszalek22 <adammarszalek60@gmail.com>
2023-10-07 19:28:16 +0100

    removed unnecessary code

--- main/classes/create_user.py
+++ main/classes/create_user.py
@@ -2,78 +2,70 @@ from api.pl_api import *
 from api.db_api import *
 
 from kivymd.app import MDApp
-from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
+from kivy.clock import Clock, mainthread
 from threading import Thread
 
-
 class CreateUser(Screen):
-    is_thread_finished = 0
 
     def do_create_user(self):
         
-        # Creating a new thread for the create_user_app function so that the spinner can be started
-        # Once create_user_app function is finished, is_thread_finished will be set to 1
-        # And then change_screen function will take effect
-        # This makes it much smoother than using the Clock.schedule_once() function on its own
-        self.ids.spinner.active = True
-        thread = Thread(target=self.create_user_app)
-        thread.start()
-        Clock.schedule_once(self.change_screen, 0.01)
-
-    def create_user_app(self):
-
         self.ids.info.text = ''
+
+        # using threading to allow the spinner to work
+        self.ids.spinner.active = True
+
+        thread = Thread(target=self.create_the_user)
+        thread.start()
+
+    def create_the_user(self):
+
+        app = MDApp.get_running_app()
+
         username = self.ids.login.text
         password = self.ids.password.text
         password2 = self.ids.password2.text
+
         # Trying to get data from fantasy premier league API. Cannot login if there is no data
         pl.get_data()
-        if pl.connection == True:
-            try:
-                new_user = create_user(username, password, password2)
-                code = new_user["code"]
-                if code == 201:
-                    app = MDApp.get_running_app()
-                    login_details = login(username, password)
-                    app.access_token = login_details["access_token"]
-                    app.refresh_token = login_details["refresh_token"]
-                    app.user_id = login_details["user_id"]
-                    self.is_thread_finished = 1 # go to next screen
-                elif code == 401:
-                    self.ids.password2.error = True
-                    self.is_thread_finished = 2 # stay in the current screen
-                else:
-                    self.ids.login.helper_text = 'User already exists.'
-                    self.ids.login.error = True
-                    self.is_thread_finished = 2 # stay in the current screen
-            except requests.exceptions.ConnectionError:
-                # Same as in login window. If this error comes up then internet works fine 
-                # but there's another problem (probably server not working)
-                self.ids.info.text = "There is a problem on our end. We are working hard to fix it"
-                self.is_thread_finished = 2
-            except json.decoder.JSONDecodeError:
-                self.ids.info.text = 'Unknown error'
-                self.is_thread_finished = 2
-        else:
+
+        if not pl.connection:
             self.ids.info.text = 'Please make sure you\'re connected to the internet. Then try again.'
             self.is_thread_finished = 2
 
-
-    def change_screen(self, dt):
-
-        if self.is_thread_finished == 1:
-            self.manager.get_screen('MainWindow').show_gameweek_games()
-            self.manager.get_screen('MainWindow').pl_table()
-            self.manager.get_screen('MainWindow').users_table()
-            self.manager.current = 'MainWindow'
+        try:
+            new_user = create_user(username, password, password2)
+            status_code = new_user["status_code"]
+            if status_code == 201:
+                login_details = login(username, password)
+                app.access_token = login_details["access_token"]
+                app.refresh_token = login_details["refresh_token"]
+                app.user_id = login_details["user_id"]
+                self.change_screen()
+            elif status_code == 401:
+                self.ids.password2.error = True
+                self.ids.spinner.active = False
+            else:
+                self.ids.login.helper_text = 'User already exists.'
+                self.ids.login.error = True
+                self.ids.spinner.active = False
+        except requests.exceptions.ConnectionError:
+            # Same as in login window. If this error comes up then internet works fine 
+            # but there's another problem (probably server not working)
+            self.ids.info.text = "There is a problem on our end. We are working hard to fix it"
             self.ids.spinner.active = False
-            self.is_thread_finished = 0
-        elif self.is_thread_finished == 2:
+        except json.decoder.JSONDecodeError:
+            self.ids.info.text = 'Unknown error'
             self.ids.spinner.active = False
-            self.is_thread_finished = 0
-        else:
-            Clock.schedule_once(self.change_screen, 0.01)
+
+    @mainthread
+    def change_screen(self):
+            
+        # self.manager.get_screen('MainWindow').show_gameweek_games()
+        # self.manager.get_screen('MainWindow').pl_table()
+        # self.manager.get_screen('MainWindow').users_table()
+        self.manager.current = 'MainWindow'
+        self.ids.spinner.active = False
 
     def password(self):
 
--- main/classes/login.py
+++ main/classes/login.py
@@ -2,81 +2,72 @@ from api.pl_api import *
 from api.db_api import *
 
 from kivymd.app import MDApp
-from kivy.clock import Clock
+from kivy.clock import Clock, mainthread
 from kivy.uix.screenmanager import Screen
 from threading import Thread
 
 
 class LoginWindow(Screen):
-    is_thread_finished = 0
 
     def do_log_in(self):
+        
+        self.ids.info.text = ''
 
-        # Creating a new thread for the log_in function so that the spinner can be started
-        # Once log_in function is finished, is_thread_finished will be set to 1
-        # And then change_screen function will take effect
-        # This makes it much smoother than using the Clock.schedule_once() function on its own
+        # using threading to allow the spinner to work
         self.ids.spinner.active = True
+
         thread = Thread(target=self.log_in)
         thread.start()
-        Clock.schedule_once(self.change_screen, 0.01)
         
     def log_in(self):
+            
+        app = MDApp.get_running_app()
 
-        self.ids.info.text = ''
         username = self.ids.login.text
         password = self.ids.password.text
+
         # Trying to get data from fantasy premier league API. Cannot login if there is no data
         pl.get_data()
-        if pl.connection == True:
-            try:
-                app = MDApp.get_running_app()
-                login_details = login(username, password)
-                code = login_details["code"]
-                if code == 200:
-                    app.access_token = login_details["access_token"]
-                    app.refresh_token = login_details["refresh_token"]
-                    app.user_id = login_details["user_id"]
-                    self.is_thread_finished = 1
-                elif code == 401:
-                    message = login_details["message"]
-                    if message == "User not found":
-                        self.ids.login.helper_text = "User not found"
-                        self.ids.login.error = True
-                    elif message == "Wrong password":
-                        self.ids.password.helper_text = "Wrong password"
-                        self.ids.password.error = True
-                    self.is_thread_finished = 2
-            except requests.exceptions.ConnectionError:
-                # If pl.connection returns true then device is connected to the internet. 
-                # This means there is problem on our end (e.g. server not running)
-                self.ids.info.text = "There is a problem on our end. We are trying to fix it..."
-                self.is_thread_finished = 2
-            except json.decoder.JSONDecodeError:
-                self.ids.info.text = 'Unknown error'
-                self.is_thread_finished = 2
-        else:
+        if not pl.connection:
             self.ids.info.text = 'Please make sure you\'re connected to the internet. Then try again.'
-            self.is_thread_finished = 2
+            self.ids.spinner.active = False
+
+        try:
+            login_details = login(username, password)
+            status_code = login_details["status_code"]
+            if status_code == 200:
+                app.access_token = login_details["access_token"]
+                app.refresh_token = login_details["refresh_token"]
+                app.user_id = login_details["user_id"]
+                self.change_screen()
+            elif status_code == 401:
+                message = login_details["message"]
+                if message == "User not found":
+                    self.ids.login.error = True
+                elif message == "Wrong password":
+                    self.ids.password.error = True
+                self.ids.spinner.active = False
+        except requests.exceptions.ConnectionError:
+            # If pl.connection returns true then device is connected to the internet. 
+            # This means there is problem on our end (e.g. server not running)
+            self.ids.info.text = "There is a problem on our end. We are trying to fix it..."
+            self.ids.spinner.active = False
+        except json.decoder.JSONDecodeError:
+            self.ids.info.text = 'Unknown error'
+            self.ids.spinner.active = False
             
                 
-    
-    def change_screen(self, dt):
+    @mainthread
+    def change_screen(self):
 
-        if self.is_thread_finished == 1:
-            self.manager.get_screen('MainWindow').show_gameweek_games()
-            self.manager.get_screen('MainWindow').pl_table()
-            self.manager.get_screen('MainWindow').users_table()
-            self.manager.current = 'MainWindow'
-            self.ids.spinner.active = False
-            self.is_thread_finished = 0
-        elif self.is_thread_finished == 2:
-            self.ids.spinner.active = False
-            self.is_thread_finished = 0
-        else:
-            Clock.schedule_once(self.change_screen, 0.01)
+        # self.manager.get_screen('MainWindow').show_gameweek_games()
+        # self.manager.get_screen('MainWindow').pl_table()
+        # self.manager.get_screen('MainWindow').users_table()
+        self.manager.current = 'MainWindow'
+        self.ids.spinner.active = False
         
     def password(self):
+        # changes icon of the textfield and shows/hides the password
 
         if self.ids.password.password == True:
             self.ids.password.password = False
