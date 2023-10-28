ef82df1c827790677a45c07c1daccaf08502e1ee
Adam Marszalek <adammarszalek60@gmail.com>
2023-10-28 17:32:33 +0100

    .

--- main/classes/main_window.py
+++ main/classes/main_window.py
@@ -1,4 +1,4 @@
-from api.pl_api import *
+from api.pl_api import pl
 from api.db_api import first_ten, my_user_info, get_all_users, get_all_bets_by_user_id, revoke_jwt, update_multiple_bets
 
 from kivy.properties import NumericProperty, ListProperty
@@ -14,8 +14,24 @@ from functools import partial
 from threading import Thread
 from datetime import datetime
 
+import copy
+
 def convert_date(date):
-    return f"{date[8:10]}/{date[5:7]}/{date[0:4]}"
+    months = {
+        1 : 'January',
+        2 : 'February',
+        3 : 'March',
+        4 : 'April',
+        5 : 'May',
+        6 : 'June',
+        7 : 'July',
+        8 : 'August',
+        9 : 'September',
+        10 : 'October',
+        11 : 'November',
+        12 : 'December'
+    }
+    return f"{date[8:10]}, {months[int(date[5:7])]} {date[0:4]}"
 
 def has_passed(start_time):
     return start_time < datetime.now()
@@ -232,7 +248,7 @@ class MainWindow(Screen):
             self.store_widgets()
 
         # display the gameweek header
-        self.handle_header()
+        self.display_header()
         
         # show matches, scores and times from the particular gameweek
         self.show_games()
@@ -251,21 +267,31 @@ class MainWindow(Screen):
             for week in pl.matches.keys():
                 for match_id in pl.matches[week].keys():
                     if pl.matches[week][match_id]["started"] == False:
+                        # the first gameweek that has a match which has not yet started is the current gameweek
                         self.gameweek = week
+                        self.current_gameweek = week
                         raise Found
         except Found:
             pass
+    
+    def previous_gameweek(self):
+        if self.gameweek != 1:
+            self.gameweek -= 1
+
+    def next_gameweek(self):
+        if self.gameweek != 38:
+            self.gameweek += 1
     
     def store_widgets(self):
 
         # storing all box_layouts in a dict for easier access
         self.widgets = {}
 
-        i = 19
-        for box_layout in self.ids.bet_layout.ids.main.children:
+        i = 19 # last widget index
+        for box_layout in self.ids.bet_layout.ids.main.children: # the loop starts with the last widget for some reason
             self.widgets[i] = {}
             self.widgets[i]['box'] = box_layout
-            self.widgets[i]['hour'] = box_layout.children[0]
+            self.widgets[i]['time'] = box_layout.children[0]
 
             self.widgets[i]['team1'] = box_layout.children[1].children[5]
             self.widgets[i]['goals1'] = box_layout.children[1].children[4]
@@ -276,37 +302,20 @@ class MainWindow(Screen):
             
             i -= 1
     
-    def bind_textfields(self):
-
-        for i in range(0, len(self.match_id_list)):
-            self.widgets[i]['prediction1'].bind(on_text_validate=partial(self.store_text_input, i))
-            self.widgets[i]['prediction2'].bind(on_text_validate=partial(self.store_text_input, i))
-
-    def previous_gameweek(self):
-
-        if self.gameweek == 1:
-            pass
-        else:
-            self.gameweek -= 1
-
-
-    def next_gameweek(self):
-
-        if self.gameweek == 38:
-            pass
-        else:
-            self.gameweek += 1
-    
-    def handle_header(self):
+    def display_header(self):
         self.ids.bet_layout.ids.header.text = f'Gameweek {self.gameweek}'
-    
+        if self.gameweek == self.current_gameweek:
+            self.ids.bet_layout.ids.header.bold = True
+        else:
+            self.ids.bet_layout.ids.header.bold = False
+
     def show_games(self):
         # show all games and their scores
 
         for i, match_id in enumerate(self.match_id_list):
             match = pl.matches[self.gameweek][match_id]
 
-            self.widgets[i]['hour'].text = f"{match['kickoff_time'][0:-3]} {convert_date(match['kickoff_date'])}"
+            self.widgets[i]['time'].text = f"{match['kickoff_time'][0:-3]} {convert_date(match['kickoff_date'])}"
             self.widgets[i]['team1'].text = pl.teams[match['team1']]["name"]
             self.widgets[i]['goals1'].text = str(match['goals1'])
             self.widgets[i]['goals2'].text = str(match['goals2'])
@@ -314,7 +323,7 @@ class MainWindow(Screen):
         
         # clearing the rest of the widgets
         for j in range(i + 1, 20):
-            self.widgets[j]['hour'].text = ''
+            self.widgets[j]['time'].text = ''
             self.widgets[j]['team1'].text = ''
             self.widgets[j]['goals1'].text = ''
             self.widgets[j]['goals2'].text = ''
@@ -323,14 +332,22 @@ class MainWindow(Screen):
     # has to be on main thread to update Kivy graphics
     @mainthread
     def show_previous_predictions(self):
-        
+        # if predicted scores were already submitted by user, show them
         app = MDApp.get_running_app()
 
         if not self.bets:
+
+            # this dict holds users predictions and will be updated 
+            # every time the user makes a change
             self.bets = get_all_bets_by_user_id(app.access_token)
 
+            # this is the original that the bets dict will be compared to
+            # when choosing what colour the textfield should be
+            # (green if the text has been changed, black otherwise)
+            self.original = copy.deepcopy(self.bets)
+
         if self.bets["status_code"] != 200:
-            return
+            return # TODO create a popup
         
         # clearing the textfields
         for i in range(20):
@@ -341,24 +358,60 @@ class MainWindow(Screen):
         for i, match_id in enumerate(self.match_id_list):
             self.widgets[i]["prediction1"].disabled = False
             self.widgets[i]["prediction2"].disabled = False
+            
             for bet in self.bets["list"]:
+                
                 if match_id == bet['match_id']:
+
+                    index = self.bets["list"].index(bet)
+                    guess = self.original["list"][index]
+
                     self.widgets[i]["prediction1"].text = str(bet['goal1'])
                     self.widgets[i]["prediction2"].text = str(bet['goal2'])
+
+                    # if the score has been amended then display it as green
+                    self.widgets[i]["prediction1"].text_color_normal = [0, 1, 0, 0.7] if str(bet['goal1']) != str(guess['goal1']) else [0, 0, 0, 1]
+                    self.widgets[i]["prediction2"].text_color_normal = [0, 1, 0, 0.7] if str(bet['goal2']) != str(guess['goal2']) else [0, 0, 0, 1]
     
-    def store_text_input(self, i, instance):
+    def bind_textfields(self):
+
+        for i in range(0, len(self.match_id_list)):
+            self.widgets[i]['prediction1'].bind(on_text_validate=partial(self.store_text_input, i))
+            self.widgets[i]['prediction2'].bind(on_text_validate=partial(self.store_text_input, i))
+
+            self.widgets[i]['prediction1'].bind(focus=partial(self.store_text_input, i))
+            self.widgets[i]['prediction2'].bind(focus=partial(self.store_text_input, i))
+    
+    def store_text_input(self, i, instance, focus = False):
+
+        if instance.focus == True:
+            return
         
         if not self.bets or not self.match_id_list:
             return
     
         for bet in self.bets["list"]:
+            
+            # if the prediction already exists
             if self.match_id_list[i] == bet['match_id']:
+
+                index = self.bets["list"].index(bet)
+                guess = self.original["list"][index]
+                
                 if instance == self.widgets[i]['prediction1']:
+                    # update to new prediction
                     bet["goal1"] = instance.text
                 else:
+                    # update to new prediction
                     bet["goal2"] = instance.text
+
+                # update colours
+                self.widgets[i]["prediction1"].text_color_normal = [0, 1, 0, 0.7] if str(bet['goal1']) != str(guess['goal1']) else [0, 0, 0, 1]
+                self.widgets[i]["prediction2"].text_color_normal = [0, 1, 0, 0.7] if str(bet['goal2']) != str(guess['goal2']) else [0, 0, 0, 1]
+
                 return
         
+        # if it's a new prediction then add it to self.bets
         if instance == self.widgets[i]['prediction1']:
             self.bets["list"].append({"match_id": self.match_id_list[i], "goal1": instance.text})
         else:
@@ -387,20 +440,26 @@ class MainWindow(Screen):
             list_guess1.append(prediction1)
             list_guess2.append(prediction2)
 
-            for match in self.bets["list"]:
+            found = False
+            for match in self.original["list"]:
                 if match["match_id"] == match_code:
                     match["goal1"] = prediction1
                     match["goal2"] = prediction2
-                    continue
+                    self.widgets[i]["prediction1"].text_color_normal = [0, 0, 0, 1]
+                    self.widgets[i]["prediction2"].text_color_normal = [0, 0, 0, 1]
+                    found = True
             
-            # if the match code doesn't exist in self.bets yet then:
-            self.bets["list"].append(
+            if found:
+                continue
+
+            # if the match code doesn't exist in self.original then add it on
+            self.original["list"].append(
                 {"match_id": match_code,
                  "goal1": prediction1,
                  "goal2": prediction2}
                 )
 
-
+        print(self.original)
         update_bets = update_multiple_bets(app.access_token, matches, list_guess1, list_guess2)
 
         if update_bets["status_code"] == 200:
@@ -442,7 +501,7 @@ class MainWindow(Screen):
     
     def logout(self):
 
-        # These are different for different users so clearing them
+        # these are different for different users so clearing them
         self.table_dialog = None
         self.bets = None
 
